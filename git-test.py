bee58747465c9e093fd2d9b4e739df4678dce62d
photonbeginner <phanvu.0708@gmail.com>
2026-03-19 22:22:55 -0400

    testing git again

---
+++ git-test.py
@@ -0,0 +1,7 @@
+def gitgity(laugh:str, suffix:str)->str:
+    laugh = input("Enter a laugh: ")
+    suffix = "gitgity"
+    return laugh + " " + suffix
+
+print(gitgity("", ""))
+# I don't know what I am doing, just test my knowledge.
