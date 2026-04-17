db22aa7afc29b506ede04d171097f2ed5ca410dc
photonbeginner <phanvu.0708@gmail.com>
2026-04-17 02:04:29 -0400

    full game added

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,24 +1,49 @@
-def rock_paper_scissor_protocol (move: str) -> str:
+import random
+# need to set the game properly
+
+def counter_move (move: str)-> str:
+# create to avoid doing all situations
     if move == "rock":
         return "paper"
     elif move == "paper":
         return "scissor"
-    elif move == "scissor":
+    else:
         return "rock"
 
-    else:
-        move=input("rock or paper or scissor? ")
-        print(rock_paper_scissor_protocol(move))
-        
-def proceed_protocol (ans: str) -> str:
-    if ans == "yes":
-        umove=input("what's your move? (rock/paper/scissor) ")
-        print(rock_paper_scissor_protocol(umove))
-    elif ans == "no":
-        print("ok :(")
-    else:
-        ans=input("yes or no? ")
-        proceed_protocol(ans)
+def play_stage (playermove: str, computermove: str) -> str:
+# need a proper game rule
+    if playermove == computermove:
+        print("Tie")
+    elif playermove == counter_move(computermove):
+        print ("You win!")
+    else: 
+        print ("You lost...")
 
-ua=input("do you wanna play rock,paper,scissor? (yes/no) ")
-proceed_protocol(ua)
+def input_valve () -> str:
+# gauge the player typed input
+    while True:
+        move=input("Choose rock, paper, or scissor: ").lower()
+        if move in ["rock", "paper", "scissor"]:
+            return move
+        print("Invalid choice. Try again")
+
+def main():
+# play the game
+    print(" Welcome back to RPS!")
+    while True:
+        pmove=input_valve()
+        cmove=random.choice(["rock", "paper", "scissor"])
+        print(f"Computer chose: {cmove}")
+        play_stage(pmove, cmove)
+
+        again=input("Wanna play again? (y/n): ")
+        if again != "y":
+            print("Thanks for playing!")
+            break
+
+while True:
+    q1=input("Wanna play r-p-scissor? (y/n): ").lower()
+    if q1 == "y":
+        main()
+        break
+    break
